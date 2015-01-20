ef893273187c359888e28516b4af78273dc09a41
Joe Letts <git@letts.me>
2015-01-19 21:09:30 -0500

    Adds correct open attr for file

--- generate.py
+++ generate.py
@@ -61,8 +61,7 @@ im = Image.fromarray(counter)
 im = im.convert('RGB')
 im.save("Mandelbrot.png")
 
-fname = "{0}.{1}.{2}.npy".format(str(dpix), str(dpiy),
-        datetime.now().strftime("%d%m%y.%H%M%S"))
-f = open(fname)
+fname = "{0}.{1}.{2}.npy".format(str(dpix), str(dpiy), datetime.now().strftime("%d%m%y.%H%M%S"))
+f = open(fname, 'wb')
 np.save(f, counter)
 f.close()
